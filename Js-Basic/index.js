16ef6daa10b788e7a6d3b92b5e2f244454c04c6b
chaitanya Tupsamudre <chaitanyatupsamudre@gmail.com>
2023-02-13 13:47:48 +0530

    JS

---
+++ Js-Basic/index.js
@@ -0,0 +1,13 @@
+console.log("namste duniya")
+
+
+let names= ["a","b","c"]
+
+// nmaes[3]='chaitya'
+// console.log(names[3])
+
+
+for(let i=0;i<5;i++)
+{
+    console.log(i)
+}
---
+++ Js-Basic/index2.js
@@ -0,0 +1,30 @@
+
+console.log("Lets start");
+
+// let rectangle = {
+//     length: 1,
+//     breadth: 2,
+
+//     draw : function(){
+//         console.log('draw')
+//     }
+// }
+
+
+function createRectangle(len,bread) {
+
+    return rectangle = {
+        length: 1,
+        breadth: 2,
+    
+        draw : function(){
+            console.log('draw')
+        }
+    };
+
+
+}
+
+let rectangleObj = createRectangle(5,4);
+
+let obj2 = createRectangle(1,2);
